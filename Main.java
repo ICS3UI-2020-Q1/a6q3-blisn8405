6757f5f7a04f25c7956f6e9a059f9e38ac3bb2ca
blisn8405 <blisn8405@wrdsb.ca>
2020-10-08 13:58:59 +0000

    q3

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  *
  * @author 
@@ -9,7 +10,24 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // crete scanner for user input
+    Scanner input= new Scanner(System.in);
+final int INTEGERS = 10;
+int number = 0;
+    //create an array with 10 spots
+    double[] user = new double[INTEGERS];
+//put integers into the array using a loop
+ System.out.println("Please enter in 10 integers into the array");
+for(int i =0; i < INTEGERS; i++){
+    user[i] = input.nextDouble();
+}
+
+// user a for loop to go through the array and find the number
+for(int i = 0; i < INTEGERS; i++){
+if(INTEGERS > user[i]){
+}
   }
+  System.out.println("The largest number is " + INTEGERS);
+
+}
 }
